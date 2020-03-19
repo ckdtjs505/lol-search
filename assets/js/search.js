89fe294a2b35b2ce8217cdf3d32ebe767b88bbfd
ckdtjs505 <ckdtjs505@naver.com>
2020-03-19 21:09:52 +0900

    검색결과 구조 변경
     - HTML을 덮어쓰는 방향으로 구현

--- assets/js/search.js
+++ assets/js/search.js
@@ -5,19 +5,20 @@ class SearchImg {
     this.render();
   }
 
-  // react set state 함수 분석
   render() {
-    // input 생성
     this.$resultSearchBox = document.querySelector(".resultImg");
+    // 2020-03-19 검색결과를 .result Img에 덮어쓰고 싶었는데..
+    // append를 하면 덮어쓰기 보다는 추가가 되었다.
+    // szHtml을 만들어서 안에 있는 html을 덮었느는 형식으로 바꿨다.
+    // 내가 원하는 모형은 아닌데 ㅠㅠ 이게 맞는지 모르겠다
     let szHtml = "";
     this.data.forEach(ele => {
       const { id, urls } = ele;
       szHtml += `
-        <div id="${id}"> 
-          <img src="${urls.thumb}"> <img/>
-        <div/>`;
+        <li > 
+          <img src="${urls.thumb}" id="${id}" > </img>
+        </li>`;
     });
-
     this.$resultSearchBox.innerHTML = szHtml;
   }
 }
