28a8a45f1210005f05171c82f4238a9ff83b23c3
ckdtjs505 <ckdtjs505@naver.com>
2020-03-20 21:35:23 +0900

    api 업데이트, search mode
     - 검색 모드가 잘 동작하도록 구현

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,11 +1,11 @@
 const api_key = "RGAPI-7380bf82-89de-4e13-8eed-5621e5461444";
 // const url = "https://kr.api.riotgames.com/lol/summoner/v4/summoners/by-name/";\
 const api_access_key = "NtxoFkRmWdaM8LtARKERKEzZp_h5B14z5jleLrQKeBg";
-// const api_secret_key = 3bvuo6nsh_d40cEw1UQcTrIQNfou0qeCx5Ucq-0FNyo;
+// const api_secret_key = "3bvuo6nsh_d40cEw1UQcTrIQNfou0qeCx5Ucq-0FNyo";
 
 const url = "https://api.unsplash.com/photos/";
 function getUserData(name) {
-  return fetch(`https://api.unsplash.com/photos?query=${name}`, {
+  return fetch(`https://api.unsplash.com/search/photos?page=1&query=${name}`, {
     headers: {
       Authorization: `Client-ID ${api_access_key}`
     }
